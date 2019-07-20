73b809416eea8c2878a4a8902938f104bb45b509
Ruterana <ncharlene38@gmail.com>
2019-07-20 11:56:48 +0200

    i add paragraph were akaname is displayed

--- js/script.js
+++ js/script.js
@@ -1,57 +1,66 @@
-var dd=parseInt(prompt("Enter your birth  date"));
-var mm=parseInt(prompt("Enter the month you born with"));
-var yyy=parseInt(prompt("Enter the year you born with"));
- var gender=prompt("Enter your gender "); 
-var cc=parseInt(prompt("Enter your century"));
-var mNames=['kwasi','kwadwo','kwabena','kwaku','yaw','kofi','kwame']; 
- 
-var d=( (( (cc/4) -2*cc-1) + ((5*yyy/4) ) + ((26*(mm+1)/10)) + dd ) %7);
-var day=Math.floor(d);
+function answerNames () {
+     var input = document.getElementById("birthdate").value;
+     var gender= document.getElementById("selection").value;
+     var cc=document.getElementById("")
+     console.log(gender);
+     var x = new Date(input);
+     yyy = x.getFullYear();
+     mm = x.getMonth();
+     mm=mm+1;
+     dd =x.getDay();
+     console.log(yyy);
+     console.log(mm);
+     console.log(dd);
+     
 
- if(day===0 && gender==='male'){
- alert("your Akaname is kwasi")
- }
- else if(day===1 && gender==='male'){ 
- alert("your Akaname is Kwadwo")
- }
- else if(day===2 && gender=='male'){
- alert("your Akaname is Kwabena")
- }
- else if(day===3 && gender==='male'){
- alert("your Akaname is Kwaku")
- }
- else if(day==4 && gender==='male'){
- alert("your Akaname is Yaw")
- }
- else if(day===5 && gender==='male'){
- alert("your Akaname is Kofi")
- }
- else if(day===6 && gender==='male'){
- alert("your Akaname is Kwame")
- }
- else if(day===0 && gender==='female'){
- alert("your Akaname is Akosua")
- }
- 
- else if(day===1 && gender==='female'){
- alert("your Akaname is Adwoa")
- }
- else if(day===2 && gender==='female'){
- alert("your Akaname is Abenaa")
- }
- else if(day===3 && gender==='female'){
- alert("your Akaname is Akua")
- 
- }
- else if(day===4 && gender=='female'){
- alert("your Akaname is Yaa")
- 
- }
- else if(day===5 && gender==='female'){
- console.log("your Akaname is Afua")
- }
- else if(day===6 && gender==='female'){
- alert("your Akaname is Ama")}
- else{
- alert("try again!")
- } 
+    var d=( (( (cc/4) -2*cc-1) + ((5*yyy/4) ) + ((26*(mm+1)/10)) + dd ) %7);
+     var day=Math.floor(d);
+
+     if(day===0 && gender==='male'){
+     document.getElementById("name").innerHTML="Kwasi"
+     }
+     else if(day===1 && gender==='male'){ 
+     document.getElementById("name").innerHTML="Kwadwo"
+     }
+     else if(day===2 && gender=='male'){
+     document.getElementById("name").innerHTML="kwabena"
+     }
+     else if(day===3 && gender==='male'){
+     document.getElementById("name").innerHTML ="Kwaku"
+     }
+     else if (day==4 && gender==='male'){
+     document.getElementById("name" ).innerHTML="Yaw"
+     }
+     else if(day===5 && gender==='male'){
+     document.getElementById("name").innerHTML = "Kofi"
+     }
+     else if(day===6 && gender==='male'){
+     document.getElementById("name").innerHTML  ="Kwame"
+    }
+     else if(day===0 && gender==='female'){
+     document.getElementById("name").innerHTML = "Akosua"
+     }
+    
+     else if(day===1 && gender==='female'){
+     Document("name").innerHTML= "Adwoa"
+     }
+     else if(day===2 && gender==='female'){
+     Document.getElementById("name") ="Abenaa"
+    }
+     else if(day===3 && gender==='female'){
+     document.getElementById("name")= "Akua"
+    
+     }
+    else if(day===4 && gender=='female'){
+    document("name") = "Yaa"
+    
+    }
+     else if(day===5 && gender==='female'){
+    document.getElementById("name") .innerHTML= "Afua"
+     }
+     else if(day===6 && gender==='female'){
+     document.getElementById("name")= "Ama"
+     
+     }
+    
+}
